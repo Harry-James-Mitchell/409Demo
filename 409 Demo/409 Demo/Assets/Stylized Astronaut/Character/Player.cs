db4c673701c46ef18a6f2e3318d24ce35b2ec3e2
Harry Mitchell <harrym@iastate.edu>
2018-11-25 13:28:42 -0600

    the project now has working timers that keep track of air levels, bettery levels, and the time the user has left to return to the ship.

--- 409 Demo/409 Demo/Assets/Stylized Astronaut/Character/Player.cs
+++ 409 Demo/409 Demo/Assets/Stylized Astronaut/Character/Player.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-		private Animator anim;
+    private float timeElapsed = 0.0f;
+    private float air = 100.0f;
+    private float bat = 100.0f;
+    private float timeLeft = 0.0f;
+    public bool moving = false;
+
+    private Animator anim;
 		private CharacterController controller;
 
 		public float speed = 600.0f;
@@ -19,17 +25,41 @@ public class Player : MonoBehaviour {
 		void Update (){
 			if (Input.GetKey ("w")) {
 				anim.SetInteger ("AnimationPar", 1);
+                moving = true;
 			}  else {
 				anim.SetInteger ("AnimationPar", 0);
+                moving = false;
 			}
 
 			if(controller.isGrounded){
 				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+                moving = true;
 			}
 
-			float turn = Input.GetAxis("Horizontal");
+            if (Input.GetAxis("Vertical")< 1)
+                { moving = false; }
+
+        float turn = Input.GetAxis("Horizontal");
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
-		}
+
+            timeElapsed += Time.deltaTime;
+            air -= Time.deltaTime / 2;
+            if (moving)
+            {
+                bat -= Time.deltaTime;
+            }
+            timeLeft = Mathf.Min(bat, air);
+        }
+
+        void OnGUI()
+        {
+            GUI.Label(new Rect(10, 10, 250, 100), "Time on mission: " + Mathf.RoundToInt(timeElapsed));
+            GUI.Label(new Rect(10, 25, 250, 100), "Air: " + Mathf.RoundToInt(air));
+            GUI.Label(new Rect(10, 40, 250, 100), "Battery: " + Mathf.RoundToInt(bat));
+            GUI.Label(new Rect(10, 55, 250, 100), "Time to return to station: " + Mathf.RoundToInt(timeLeft));
+
+
+        }
 }
