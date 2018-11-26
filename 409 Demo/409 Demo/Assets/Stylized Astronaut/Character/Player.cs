cce6b0db296229dfc5ea3efb27548b910d952555
Harry Mitchell <harrym@iastate.edu>
2018-11-26 16:46:40 -0600

    I believe I am finished with the project, I'm letting my group mateoject. I'm letting my group mates give me feed back and see if any other changes need to be made.

--- 409 Demo/409 Demo/Assets/Stylized Astronaut/Character/Player.cs
+++ 409 Demo/409 Demo/Assets/Stylized Astronaut/Character/Player.cs
@@ -8,58 +8,84 @@ public class Player : MonoBehaviour {
     private float bat = 100.0f;
     private float timeLeft = 0.0f;
     public bool moving = false;
+    private float coinsLeft = 4.0f;
+    public bool win = false;
 
     private Animator anim;
-		private CharacterController controller;
+	private CharacterController controller;
 
-		public float speed = 600.0f;
-		public float turnSpeed = 400.0f;
-		private Vector3 moveDirection = Vector3.zero;
-		public float gravity = 20.0f;
+	public float speed = 600.0f;
+	public float turnSpeed = 400.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	public float gravity = 20.0f;
 
-		void Start () {
-			controller = GetComponent <CharacterController>();
-			anim = gameObject.GetComponentInChildren<Animator>();
+	void Start () {
+		controller = GetComponent <CharacterController>();
+		anim = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	void Update (){
+		if (Input.GetKey ("w")) {
+			anim.SetInteger ("AnimationPar", 1);
+            moving = true;
+		}  else {
+			anim.SetInteger ("AnimationPar", 0);
+            moving = false;
 		}
 
-		void Update (){
-			if (Input.GetKey ("w")) {
-				anim.SetInteger ("AnimationPar", 1);
-                moving = true;
-			}  else {
-				anim.SetInteger ("AnimationPar", 0);
-                moving = false;
-			}
+		if(controller.isGrounded){
+			moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+            moving = true;
+		}
 
-			if(controller.isGrounded){
-				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-                moving = true;
-			}
+        if (Input.GetAxis("Vertical")< 1)
+            { moving = false; }
 
-            if (Input.GetAxis("Vertical")< 1)
-                { moving = false; }
+        float turn = Input.GetAxis("Horizontal");
+		transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+		controller.Move(moveDirection * Time.deltaTime);
+		moveDirection.y -= gravity * Time.deltaTime;
 
-            float turn = Input.GetAxis("Horizontal");
-			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-			controller.Move(moveDirection * Time.deltaTime);
-			moveDirection.y -= gravity * Time.deltaTime;
-
-            timeElapsed += Time.deltaTime;
-            air -= Time.deltaTime / 2;
-            if (moving)
-            {
-                bat -= Time.deltaTime;
-            }
-            timeLeft = Mathf.Min(bat, air);
+        timeElapsed += Time.deltaTime;
+        air -= Time.deltaTime / 2;
+        if (moving)
+        {
+            bat -= Time.deltaTime;
         }
+        timeLeft = Mathf.Min(bat, air);
+    }
 
-        void OnGUI()
+    void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.collider.gameObject.tag == "coin")
+        {
+            Destroy(col.collider.gameObject);
+            coinsLeft -= 1;
+        }
+        else 
+        if (col.collider.gameObject.tag == "rocket")
+        {
+            if(coinsLeft <= 0)
+            {
+                win = true;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (win)
+        {
+            GUI.Label(new Rect(200, 200, Screen.width, Screen.height), "Mission completed.");
+            Application.LoadLevel(0);
+        }
+        else
         {
             GUI.Label(new Rect(10, 10, 250, 100), "Time on mission: " + Mathf.RoundToInt(timeElapsed));
             GUI.Label(new Rect(10, 25, 250, 100), "Air: " + Mathf.RoundToInt(air));
             GUI.Label(new Rect(10, 40, 250, 100), "Battery: " + Mathf.RoundToInt(bat));
             GUI.Label(new Rect(10, 55, 250, 100), "Time to return to station: " + Mathf.RoundToInt(timeLeft));
-
-
+            GUI.Label(new Rect(10, 70, 250, 100), "Coins left to collect: " + Mathf.RoundToInt(coinsLeft));
         }
+    }
 }
